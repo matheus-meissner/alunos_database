d6a4ef41b3a7279c6aebdceab054b55849a9bfaf
Matheus Meissner <matheus.iembo@gmail.com>
2024-09-09 16:35:35 -0300

    chave_estrangeira

--- alunos_database.sql
+++ alunos_database.sql
@@ -226,6 +226,51 @@ INSERT INTO curso (id, nome) VALUES (2, 'Javascript');
 
 SELECT * FROM curso;
 
+-- ATIVIDADE 3
+DROP TABLE aluno;
+
+CREATE TABLE aluno (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL
+);
+
+INSERT INTO aluno (nome) VALUES ('Diogo');
+INSERT INTO aluno (nome) VALUES ('Vinícius');
+
+SELECT * FROM aluno;
+SELECT * FROM curso;
+
+DROP TABLE aluno_curso;
+CREATE TABLE aluno_curso (
+	aluno_id INTEGER,
+	curso_id INTEGER,
+	PRIMARY KEY (aluno_id, curso_id),
+	
+	FOREIGN KEY (aluno_id) -- PONTE ENTRE DADOS DA TABELA NOVA E ANTIGA (ponte entre inverntário e loja)
+		REFERENCES aluno (id),
+
+	FOREIGN KEY (curso_id)
+		REFERENCES curso (id)
+);
+
+
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,1);
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,1);
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (3,1); -- ALUNO INEXISTENTE
+
+SELECT * FROM aluno WHERE id = 1;
+SELECT * FROM aluno WHERE id = 1;
+
+SELECT * FROM aluno WHERE id = 2;
+SELECT * FROM aluno WHERE id = 1;
+
+
+
+
+
+
+
+
 
 
 
