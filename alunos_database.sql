990bd3161ab4532c423d7e22caa8819742f54012
Matheus Meissner <matheus.iembo@gmail.com>
2024-09-24 12:32:28 -0300

    agregacao

--- alunos_database.sql
+++ alunos_database.sql
@@ -415,9 +415,19 @@ SELECT * FROM funcionarios ORDER BY nome LIMIT 5;
 SELECT * FROM funcionarios ORDER BY id LIMIT 5 OFFSET 1;
 
 
+-- AGREGAÇÃO
+-- COUNT - Retorna a quantidade de registros
+-- SUM -   Retorna a soma dos registros
+-- MAX -   Retorna o maior valor dos registros
+-- MIN -   Retorna o menor valor dos registros
+-- AVG -   Retorna a média dos registros
 
-
-
+SELECT COUNT (id),
+       SUM(id),
+       MAX(id),
+       MIN(id),
+       ROUND(AVG(id),0)
+  FROM funcionarios;
 
 
 
